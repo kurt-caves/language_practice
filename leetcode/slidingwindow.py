e7619cba9939663267259edd224a0f12139919c3
Kurt Lyell <kurtly@nmsu.edu>
2024-10-16 07:48:00 -0600

    more sliding window

--- leetcode/slidingwindow.py
+++ leetcode/slidingwindow.py
@@ -7,3 +7,21 @@ window_size = 3
 
 for window in sliding_window(data, window_size):
     print(window)
+
+def sliding_window(iterable, window_size):
+    result = []
+    length = len(iterable)
+    for start in range(length):
+        for end in range(start, length):
+            window = []
+            for index in range(start, end + 1):
+                window.append(iterable[index])
+            if len(window) == window_size:
+                result.append(tuple(window))
+    return result
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+windows = sliding_window(data, 3)
+for window in windows:
+    print(window)
