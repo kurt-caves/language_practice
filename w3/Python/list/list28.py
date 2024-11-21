c1b018b59ee8d07768036091d27703407c5e0599
Kurt Lyell <kurtly@nmsu.edu>
2024-11-21 08:23:13 -0700

    working on python lists

--- w3/Python/list/list28.py
+++ w3/Python/list/list28.py
@@ -1,1 +1,13 @@
 '''28. Write a Python program to find the second largest number in a list.'''
+import math
+nums = [1,2,3,4,5,6,7,8,9]
+max1 = float('-inf')
+max2 = float('-inf')
+for num in nums:
+    if num > max1:
+        max1 = num
+for num in nums:
+    if num > max2 and num < max1:
+        max2 = num
+print(max1)
+print(max2)
