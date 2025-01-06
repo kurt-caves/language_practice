6755a6b19920ad2a7caf838ae143856bb268a4be
kurtly <kurtly@nmsu.edu>
2025-01-06 08:20:50 -0700

    working on haskell

---
+++ haskell/starting_point/higher_order.hs
@@ -0,0 +1,9 @@
+-- map applies a function to every element of a list and returns a new list with the results
+-- filter applies a function to every element of a list and returns a new list
+-- foldr and foldl applies a function from right or left of list all the way to the left or the right
+-- sort -> sorts a list
+-- zipWith * / + - two lists of equal length lists
+-- takeWhile takes elements from a list while a condition is true and returns them
+-- dropWhile deletes elements from a list while a condition is true
+-- all checks a condition for a list retruns a boolean
+-- any checks check a condition for a list returns a boolean
